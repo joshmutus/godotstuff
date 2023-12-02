e29590c5ef00c8a5f85765b313339c5182b55093
joshmutus <josh.mutus@gmail.com>
2023-12-02 10:10:20 -0800

    Initial commit

---
+++ car.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+var wheel_base = 70
+var steering_angle = 15
+var engine_power = 800
+var friction = -0.6
+var drag = -0.001
+var braking = -450
+var max_speed_reverse = 250
+var slip_speed = 400
+var traction_fast = 0.0
+var traction_slow = 0.8
+
+var steer_direction
+
+var acceleration = Vector2.ZERO
+
+
+func _process(delta):
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction()
+	print("velocity, ", velocity.length())
+	calculate_steering(delta)
+	velocity += acceleration * delta
+	move_and_slide()
+	
+func apply_friction():
+	if velocity.length() < 10:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction
+	var drag_force = velocity * velocity.length() * drag
+	acceleration += drag_force + friction_force
+	
+func get_input():
+	var turn = 0
+	if Input.is_action_pressed("steer_right"):
+		turn += 1 
+	if Input.is_action_pressed("steer_left"):
+		turn -= 1
+	steer_direction = turn*deg_to_rad(steering_angle)
+	if Input.is_action_pressed("gas"):
+		velocity = transform.x * engine_power
+	if Input.is_action_pressed("brake"):
+		acceleration = transform.x * braking
+func lin_interp(num1, num2):
+	return (num1+num2)/2		
+		
+func calculate_steering(delta):
+	var rear_wheel = position - transform.x*wheel_base/2
+	var front_wheel = position + transform.x*wheel_base/2
+	rear_wheel += velocity*delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	var traction = traction_slow
+	if velocity.length() > slip_speed:
+		traction = traction_fast
+	print("traction: ", traction)
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0:
+		#velocity = new_heading*velocity.length()
+		velocity = velocity.lerp(new_heading*velocity.length(), traction)
+		print(new_heading*velocity.length() - velocity)
+	if d < 0:
+		velocity = -new_heading*min(velocity.length(), max_speed_reverse)		
+	rotation = new_heading.angle()
+		
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
