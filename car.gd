af36e6498f585de0815c8f1eb0e70e468f4eb96e
joshmutus <josh.mutus@gmail.com>
2023-12-09 18:07:13 -0800

    Add terrain tiles

--- car.gd
+++ car.gd
@@ -13,6 +13,9 @@ var traction_slow = 0.8
 var steer_direction
 var acceleration = Vector2.ZERO
 
+var surfaces = {0: "grass", 1: "asphalt", 2: "dirt", 3: "mud"}
+
+
 func _process(delta):
 	acceleration = Vector2.ZERO
 	get_input()
@@ -21,50 +24,59 @@ func _process(delta):
 	calculate_steering(delta)
 	velocity += acceleration * delta
 	move_and_slide()
-	
+
+
 func apply_friction():
+	for i in get_slide_collision_count():
+		var col = get_slide_collision(i)
+		print(col.get_collider().name)
 	if velocity.length() < 10:
 		velocity = Vector2.ZERO
 	var friction_force = velocity * friction
 	var drag_force = velocity * velocity.length() * drag
 	acceleration += drag_force + friction_force
-	
+
+
 func get_input():
 	var turn = 0
 	if Input.is_action_pressed("steer_right"):
-		turn += 1 
+		turn += 1
 	if Input.is_action_pressed("steer_left"):
 		turn -= 1
-		
+
 	steer_direction = turn * deg_to_rad(steering_angle)
-	
+
 	if Input.is_action_pressed("gas"):
 		velocity = transform.x * engine_power
 	if Input.is_action_pressed("brake"):
 		acceleration = transform.x * braking
 
+
 func lin_interp(num1, num2):
-	return (num1 + num2) / 2		
-		
+	return (num1 + num2) / 2
+
+
 func calculate_steering(delta):
-	var rear_wheel = position - transform.x * wheel_base/2
-	var front_wheel = position + transform.x * wheel_base/2
+	var rear_wheel = position - transform.x * wheel_base / 2
+	var front_wheel = position + transform.x * wheel_base / 2
 	rear_wheel += velocity * delta
 	front_wheel += velocity.rotated(steer_direction) * delta
 	var new_heading = (front_wheel - rear_wheel).normalized()
 	var traction = traction_slow
+	%fw_rect.color = Color(1.0, 1.0, 1.0, 1.0)
 	if velocity.length() > slip_speed:
+		%fw_rect.color = Color(1.0, 0.0, 0.0, 1.0)
 		traction = traction_fast
-	print("traction: ", traction)
+		print("slipping traction: ", traction)
 	var d = new_heading.dot(velocity.normalized())
 	if d > 0:
-		#velocity = new_heading*velocity.length()
+		# velocity = new_heading*velocity.length()
 		velocity = velocity.lerp(new_heading * velocity.length(), traction)
-		print(new_heading * velocity.length() - velocity)
+		print(rad_to_deg(new_heading.angle()), new_heading * velocity.length() - velocity)
 	if d < 0:
-		velocity = -new_heading * min(velocity.length(), max_speed_reverse)		
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
 	rotation = new_heading.angle()
-		
+
 
 func _ready():
-	pass # Replace with function body.
+	pass  # Replace with function body.
